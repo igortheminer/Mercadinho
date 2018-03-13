f8868063048cae7eb4e686e78105f4c559432420
Igor Mateus Inglêz <37289388+igortheminer@users.noreply.github.com>
2018-03-13 19:14:34 -0300

    corrigido alguns erros
    corrigido alguns erros com a função modf_arq_senha(void), que estava salvando valores indesejados, e na função cria_arq_senha(void), que agora caso tenha um valor ilegível no arquivo (apagando/inutilizando a senha salva), essa função irá substituí-lo pela senha padrão

--- login.c
+++ login.c
@@ -8,9 +8,10 @@
 char keyboard[BUFSIZ];/** para limpeza de buffer ... setbuf(stdin, keyboard); **/
 
 void cria_arq_senha(void){/** CASO NAO EXISTA O ARQUIVO "senha.dat" ESSA FUNCAO IRA CRIAR UM COM A SENHA PADRAO "123" **/
+	int verifica;
+	unsigned int senha_arq;
 	FILE *arq = fopen("senha.dat", "rb");
 	unsigned int senha_padrao = SENHA_PADRAO;
-	int verifica;
 
 	if(arq == NULL){/** VERIFICA SE O ARQUIVO EXISTE, E CASO NAO EXISTA E CRIADO UM **/
 		arq = fopen("senha.dat", "wb");
@@ -21,17 +22,29 @@ void cria_arq_senha(void){/** CASO NAO EXISTA O ARQUIVO "senha.dat" ESSA FUNCAO 
 			exit(1);
 		}
 	}
+	verifica = fread(&senha_arq, sizeof(unsigned int), 1, arq);
 	fclose(arq);
+	if(verifica != 1){/** ASSEGURA QUE O ARQUIVO SEJA SIDO LIDO CORETAMENTE **/
+		arq = fopen("senha.dat", "wb");
+		verifica = fwrite(&senha_padrao, sizeof(unsigned int), 1, arq);
+		fclose(arq);
+		if(verifica != 1){/** CASO OCORRA UM PROBLEMA EXECUTA ISSO **/
+			perror("ERRO NA ESCRITA DO ARQUIVO <senha.dat>");
+			getch();
+			exit(1);
+		}
+	}
 }
 
 void modf_arq_senha(void){/** MUDA A SENHA SALVA EM "senha.dat" **/
 	unsigned int nova_senha;
 	int verifica;
-	FILE *arq = fopen("senha.dat", "wb");
+	FILE *arq;
 
 	printf("\nDIGITE A NOVA SENHA (apenas numeros): ");
 	setbuf(stdin, keyboard);
 	scanf("%u", &nova_senha);
+	arq = fopen("senha.dat", "wb");
 	verifica = fwrite(&nova_senha, sizeof(unsigned int), 1, arq);
 	if(verifica != 1){/** CASO OCORRA UM PROBLEMA EXECUTA ISSO **/
 		perror("ERRO NA ESCRITA DO ARQUIVO <senha.dat>");
@@ -54,7 +67,7 @@ int gera_login(void){/** FAZ LOGIN, CASO A SENHA DIGITADA SEJA INCORRETA RETORNA
 	}
 
 	verifica = fread(&senha_arq, sizeof(unsigned int), 1, arq);
-	if(verifica != 1){/** ASSEGURA QUE O ARQUIVO ENHA SIDO LIDO CORETAMENTE **/
+	if(verifica != 1){/** ASSEGURA QUE O ARQUIVO TENHA SIDO LIDO CORETAMENTE **/
 		perror("ERRO NA LEITURA DO ARQUIVO <senha.dat>");
 		getch();
 		exit(2);
